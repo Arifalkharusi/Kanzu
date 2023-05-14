d8fba988e25bf1b87f22d521c82989b5c1dad53a
Arif Al Kharusi <arifalkharusi@hotmail.com>
2023-05-14 11:15:02 +0100

    14/05/23 main

--- frontend/src/components/Items/Items.jsx
+++ frontend/src/components/Items/Items.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import style from "./Items.module.scss";
 import Product from "../Product/Product";
 // import data from "../../data/data.json";
+import { TailSpin } from "react-loader-spinner";
 
 const Items = (props) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://kanzu-api.onrender.com/api/admin/get-product")
+    fetch("/api/admin/get-product")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
@@ -37,11 +38,26 @@ const Items = (props) => {
           </div>
         </div>
       </div>
-      <div className={style.listings}>
-        {data.map((x, i) => (
-          <Product data={x} key={i} />
-        ))}
-      </div>
+      {data[0]?._id ? (
+        <div className={style.listings}>
+          {data.map((x, i) => (
+            <Product data={x} key={i} />
+          ))}
+        </div>
+      ) : (
+        <div className={style.loader}>
+          <TailSpin
+            height="50"
+            width="50"
+            color="black"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      )}
     </div>
   );
 };
